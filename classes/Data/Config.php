50498906aba51ea30a6b2840faaa630a8334c01d
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2012-02-29 04:06:28 +0100

    Added environment specific config support.

--- classes/Data/Config.php
+++ classes/Data/Config.php
@@ -12,12 +12,22 @@ class Config extends \Classes\Data\Base
 	 */
 	public function load($file)
 	{
+		// Find the main files
 		$files = $this->_app->find_files('config', $file);
 		foreach ($files as $file)
 		{
 			$array = require $file;
 			$this->_data = array_merge($this->_data, $array);
 		}
+
+		// Find optional overwrites
+		$this->_app->find_files('config/'._env('name'), $file);
+		foreach ($files as $file)
+		{
+			$array = require $file;
+			$this->_data = array_merge($this->_data, $array);
+		}
+
 		return $this;
 	}
 }
